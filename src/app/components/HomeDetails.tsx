f48fdbec54d7577d7f4462689e4851433917767b
Abhay Kaushal <abhaykaushal920@gmail.com>
2025-05-13 12:00:10 +0530

    update resume

--- src/app/components/HomeDetails.tsx
+++ src/app/components/HomeDetails.tsx
@@ -30,7 +30,7 @@ export default function HomeDetails() {
                 <TextGenerateEffect words={words} />
 
                 <div className="mt-4">
-                    <Link href="https://drive.google.com/file/d/1cdDTs3dBbxNlaTzkgdqPVPEeY2OoSMfr/view?usp=drivesdk">
+                    <Link href="https://drive.google.com/file/d/1b51KV81oWJlNSiweN6PeEvYKkgOtLkEq/view?usp=drive_link">
                         <Button
                             borderRadius="1.75rem"
                             className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
